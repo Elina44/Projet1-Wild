3e9b6d0b6ecee9a50a3c7203c7612cb467ba2ad3
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-05-13 21:22:28 +0200

    mise en place de la galerie pour les plantes à réserve

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ icon.addEventListener("click", function(){
     list.classList.toggle("list-visible");
     //on rappelle la classe pour qu'il s'affiche
 })
+
+//Essai mise en place de la galerie page2
+
+//Je sélectionne les petites photos et les stocke dans un tableau
+const smallPictures = document.querySelectorAll(".small");
+console.log(smallPictures);//Je sélectionne l'image grand format et la stocke
+const fullPicture = document.getElementById("pivoine");smallPictures.forEach((item) => {
+    item.addEventListener("click", function(){
+        let imgSource = item.getAttribute("src");
+        fullPicture.setAttribute("src", imgSource);
+    });
+});
